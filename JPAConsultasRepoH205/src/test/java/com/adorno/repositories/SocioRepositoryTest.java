1f89e478ab334886ee34e6c56d201b6a7254c92c
pabloieshna <joesingleton417@gmail.com>
2024-01-22 12:18:24 +0100

    pruebas repo 2

--- JPAConsultasRepoH205/src/test/java/com/adorno/repositories/SocioRepositoryTest.java
+++ JPAConsultasRepoH205/src/test/java/com/adorno/repositories/SocioRepositoryTest.java
@@ -1,5 +1,6 @@
 package com.adorno.repositories;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.fail;
 
 import java.time.LocalDate;
@@ -27,17 +28,28 @@ class SocioRepositoryTest {
 	        listaSocios.add(new Socio(false, LocalDate.of(2022, 8, 25), "López", "Sofía", 150.0f));
 	        listaSocios.add(new Socio(true, LocalDate.of(2023, 2, 15), "Fernández", "Alejandro", 200.0f));
 	        listaSocios.add(new Socio(false, LocalDate.of(2022, 11, 3), "Rodríguez", "Laura", 120.0f));
-	        listaSocios.add(new Socio(true, LocalDate.of(2023, 1, 20), "Gómez", "Pablo", 80.0f));
+	        listaSocios.add(new Socio(true, LocalDate.of(2023, 1, 1), "Gómez", "Pablo", 80.0f));
 	        listaSocios.add(new Socio(false, LocalDate.of(2023, 4, 5), "Díaz", "Ana", 130.0f));
 	        listaSocios.add(new Socio(true, LocalDate.of(2022, 10, 15), "Martínez", "Carlos", 90.0f));
 	        listaSocios.add(new Socio(true, LocalDate.of(2022, 7, 8), "Pérez", "Elena", 110.0f));
 	        listaSocios.add(new Socio(false, LocalDate.of(2023, 3, 1), "González", "Javier", 160.0f));
 	        listaSocios.add(new Socio(true, LocalDate.of(2022, 12, 10), "Sánchez", "Isabel", 70.0f));
 	        listaSocios.add(new Socio(false, LocalDate.of(2023, 6, 18), "Romero", "Francisco", 180.0f));
+	        socioRepository.saveAll(listaSocios);
 	}
+	
 	@Test
 	void test() {
-		fail("Not yet implemented");
+		int activos=6;
+		boolean activo = true;
+		assertEquals(activos, socioRepository.findAllByActivo(activo).size());
+		LocalDate of = LocalDate.of(2022,12,31);
+		assertEquals(activos, 
+				socioRepository
+				.findAllByFechaAdscripcionAfter(of).size());
+		int apellidosSuecos=8;
+		String terminacionSueca="ez";
+		assertEquals(apellidosSuecos, socioRepository.findAllByApellidosEndsWith(terminacionSueca).size());
 	}
 
 }
