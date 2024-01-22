fe76d6cd3dc6c164b8c213bbacfaf6980269e269
pabloieshna <joesingleton417@gmail.com>
2024-01-22 11:43:02 +0100

    repos consultas

---
+++ JPAConsultasRepoH205/src/main/java/com/adorno/JpaConsultasRepoH205Application.java
@@ -0,0 +1,13 @@
+package com.adorno;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpaConsultasRepoH205Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JpaConsultasRepoH205Application.class, args);
+	}
+
+}
---
+++ JPAConsultasRepoH205/src/main/java/com/adorno/modelo/Socio.java
@@ -0,0 +1,12 @@
+package com.adorno.modelo;
+
+import java.time.LocalDate;
+
+public class Socio {
+	private Long id;
+	private boolean activo;
+	private LocalDate fechaAdscripcion;
+	private String apellidos,nombre;
+	private float saldo;
+	
+}
